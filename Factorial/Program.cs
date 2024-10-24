535830c9ea7c7b0aee439b384991590720b64e8f
driehajl <driehajl@mail.uc.edu>
2024-10-24 11:54:58 -0400

    Added documentatoin

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/*
+* Name: Jack Driehaus
+* email:  driehajl@mail.uc.edu
+* Assignment Number: Assignment in class 10/24/24
+* Due Date:   10/24/24
+* Course #/Section:   IS3050-001
+* Semester/Year:   Fall 2024
+* Brief Description of the assignment:  Create a factorial calculator 
+
+* Brief Description of what this module does. {Do not copy/paste from a previous assignment. Put some thought into this. required}
+* Citations:
+* Anything else that's relevant:
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +26,11 @@ namespace Factorial
         {
             Console.WriteLine(ComputeFactorial(4));
         }
+        /// <summary>
+        /// Factorial calculator
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
 
         static int ComputeFactorial(int num)
         {
